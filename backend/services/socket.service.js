895b31214c02714832d72c6417f47306c114f2dd
jack <jack.nguyen@weareday.one>
2023-07-04 16:20:43 +0700

    feat: update player position on server

--- backend/services/socket.service.js
+++ backend/services/socket.service.js
@@ -65,11 +65,14 @@ export const connection = (socket) => {
   // new logic
   const { userId } = socket;
   if (!backEndPlayers[userId]) {
+    const x = randomNumber(0, maxX);
+    const y = randomNumber(0, maxY);
     backEndPlayers[userId] = {
-      x: randomNumber(0, maxX),
-      y: randomNumber(0, maxY),
+      x,
+      y,
       color: `hsl(${360 * Math.random()}, 100%, 50%)`,
       sequenceNumber: 0,
+      direction: 'right',
     };
   }
 
@@ -80,23 +83,64 @@ export const connection = (socket) => {
     switch (keycode) {
       case 'KeyW':
         backEndPlayers[userId].y -= speed;
+        backEndPlayers[userId].direction = 'up';
         break;
 
       case 'KeyA':
         backEndPlayers[userId].x -= speed;
+        backEndPlayers[userId].direction = 'left';
         break;
 
       case 'KeyS':
         backEndPlayers[userId].y += speed;
+        backEndPlayers[userId].direction = 'down';
         break;
 
       case 'KeyD':
         backEndPlayers[userId].x += speed;
+        backEndPlayers[userId].direction = 'right';
         break;
     }
   });
 };
 
-setInterval(() => _io.emit('updatePlayers', backEndPlayers), tickRate);
-
 const backEndPlayers = {};
+
+const updatePlayerPositions = () => {
+  for (const id in backEndPlayers) {
+    const player = backEndPlayers[id];
+    switch (player.direction) {
+      case 'up':
+        player.y -= speed;
+        break;
+      case 'down':
+        player.y += speed;
+        break;
+      case 'left':
+        player.x -= speed;
+        break;
+      case 'right':
+        player.x += speed;
+        break;
+    }
+  }
+};
+
+const checkPlayers = () => {
+  for (const id in backEndPlayers) {
+    const player = backEndPlayers[id];
+  }
+};
+
+const gameTick = () => {
+  // implement logic check if game over
+  updatePlayerPositions(); // snake game is a game requires movements without user interaction
+  _io.emit('updatePlayers', backEndPlayers);
+};
+
+const gameTickInterval = setInterval(gameTick, tickRate);
+
+// we got 3 types of realtime games
+// turn based: chess, monopoly,...
+// game state changes when user interacts: racing,...
+// game state changes without user interactions: snake
